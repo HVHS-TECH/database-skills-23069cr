2d1620b3740e5e12d0ce95e0de8fe4fdebcf8c16
23069cr <23069cr@hvhs.school.nz>
2026-04-24 00:01:57 +0000

    d

--- fb_setup.js
+++ fb_setup.js
@@ -10,17 +10,17 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCk102EjA3cGbhDOmPXucRhwaLa_OCKvPQ",
   authDomain: "cobyrollo-12comp.firebaseapp.com",
-  databaseURL: "https://cobyrollo-12comp-default-rtdb.firebaseio.com",
+  databaseURL: "https://cobyrollo-12comp-default-rtdb.firebaseio.com/",
   projectId: "cobyrollo-12comp",
   storageBucket: "cobyrollo-12comp.firebasestorage.app",
   messagingSenderId: "336583454402",
   appId: "1:336583454402:web:fc9d396e2dace7f3ea155b"
 };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // This log prints the firebase object to the console to show that it is working.
-  // As soon as you have the script working, delete this log.
-  console.log("Firebase initialize finished:");
-  console.log(firebase);
+// This log prints the firebase object to the console to show that it is working.
+// As soon as you have the script working, delete this log.
+console.log("Firebase initialize finished:");
+console.log(firebase);
