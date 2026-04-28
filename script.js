8867d0270d21d96350e477b3ac9f5d68ef8b4d18
23069cr <23069cr@hvhs.school.nz>
2026-04-28 03:10:08 +0000

    Message read

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // The ref('/') part tells the operation to write to the base level of the database "/"
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
-function helloWorld(){
+function helloWorld() {
+  firebase.database().ref("/message").once("value", DO_THIS)
   console.log("Running helloWorld()")
   firebase.database().ref('/').set(
     {
@@ -26,7 +27,39 @@ function helloWorld(){
   )
 }
 
-function goodbye(){
+function DO_THIS(snapshot) {
+  console.log(snapshot.val());
+}
+
+function simpleRead() {
+  console.log("Reading message");
+  firebase.database().ref('/').child('message').once('value', displayRead);
+  console.log("Leaving simpleRead")
+}
+
+function displayRead(snapshot) {
+  console.log("Running displayRead(), the message is: " + snapshot.val())
+  HTML_OUTPUT.innerHTML = snapshot.val();
+}
+
+
+function display(snapshot) {
+  var dbData = snapshot.val();
+  if (dbData == null) {
+    console.log('No message stupid');
+  }
+  else {
+    console.log("The message is: " + dbData)
+  }
+}
+
+function fb_readError(error) {
+  firebase.database().ref('/').child('message').once('value', displayfb_readError);
+  console.log("There was an error reading the message");
+  console.error(error);
+}
+
+function goodbye() {
   console.log("Running goodbye()")
   firebase.database().ref('/').set(
     {
