13c72368c252013e34dcfbf57d70f54fb2e1d420
23069cr <23069cr@hvhs.school.nz>
2026-05-08 00:27:48 +0000

    Did some sorting

--- fb_io.js
+++ fb_io.js
@@ -12,21 +12,22 @@
 // This function reads the high scores for game1 and logs them to the console//
 function fb_readHighScores() {
     console.log("Reading High scores");
-    firebase.database().ref('game1/users')
-        .once('value', fb_displayHighScores, fb_readError);
+    firebase.database().ref('game2/users') .once('value', fb_displayHighScores, fb_readError);
+}
+
+function fb_displayLowestScores() {
+    console.log("Reading Lowest scores");
+    firebase.database().ref('game2/users') .orderByValue() .once('value', fb_displayHighScores, fb_readError);
 }
 
 function fb_displayHighScores(snapshot) {
     snapshot.forEach(fb_showOneScore);
-
 }
 
 //reads each child's score//
 function fb_showOneScore(child) {
     console.log(child.key + " got " + child.val() + " points");
 }
-
-
 
 function fb_logDatabaseRead(snapshot) {
     let data = snapshot.val();
@@ -55,7 +56,7 @@ function displayRead(snapshot) {
 
 //error stuff//
 function fb_readError(error) {
-    firebase.database().ref('/').child('message').once('value', displayfb_readError);
+    firebase.database().ref('/').child('message').once('value', fb_readError);
     console.log("There was an error reading the message");
     console.error(error);
 }
